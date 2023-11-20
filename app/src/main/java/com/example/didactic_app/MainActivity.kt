26a23dafb572e7032a78b06b49f76736f63b7757
Iker Gonzalez <nintzatekeen@gmail.com>
2023-11-20 10:54:16 +0100

    Commit inicial; probando osmdroid

---
+++ app/src/main/java/com/example/didactic_app/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.didactic_app
+
+import android.graphics.Rect
+import android.location.GpsStatus
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.didactic_app.databinding.ActivityMainBinding
+import org.osmdroid.api.IMapController
+import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapListener
+import org.osmdroid.events.ScrollEvent
+import org.osmdroid.events.ZoomEvent
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
+class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
+
+
+
+    lateinit var mMap: MapView
+    lateinit var controller: IMapController;
+    lateinit var mMyLocationOverlay: MyLocationNewOverlay;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Configuration.getInstance().load(
+            applicationContext,
+            getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        )
+        mMap = binding.osmmap
+        mMap.setTileSource(TileSourceFactory.MAPNIK)
+        mMap.mapCenter
+        mMap.setMultiTouchControls(true)
+        mMap.getLocalVisibleRect(Rect())
+
+
+        mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
+        controller = mMap.controller
+
+        mMyLocationOverlay.enableMyLocation()
+        mMyLocationOverlay.enableFollowLocation()
+        mMyLocationOverlay.isDrawAccuracyEnabled = true
+        mMyLocationOverlay.runOnFirstFix {
+            runOnUiThread {
+                controller.setCenter(mMyLocationOverlay.myLocation);
+                controller.animateTo(mMyLocationOverlay.myLocation)
+            }
+        }
+        // val mapPoint = GeoPoint(latitude, longitude)
+
+        controller.setZoom(6.0)
+
+        Log.e("TAG", "onCreate:in ${controller.zoomIn()}")
+        Log.e("TAG", "onCreate: out  ${controller.zoomOut()}")
+
+        // controller.animateTo(mapPoint)
+        mMap.overlays.add(mMyLocationOverlay)
+
+        mMap.addMapListener(this)
+
+
+    }
+
+    override fun onScroll(event: ScrollEvent?): Boolean {
+        // event?.source?.getMapCenter()
+        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
+        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
+        //  Log.e("TAG", "onScroll   x: ${event?.x}  y: ${event?.y}", )
+        return true
+    }
+
+    override fun onZoom(event: ZoomEvent?): Boolean {
+        //  event?.zoomLevel?.let { controller.setZoom(it) }
+
+
+        Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
+        return false;
+    }
+
+    override fun onGpsStatusChanged(event: Int) {
+
+
+        TODO("Not yet implemented")
+    }
+
+
+}
