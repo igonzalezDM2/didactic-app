e298e015e8c90b4ff935a11d5b8e4e8d77565371
Iker Gonzalez <nintzatekeen@gmail.com>
2023-11-24 12:26:58 +0100

    Menú provisional con botones para ir haciendo las aplicaciones

--- app/src/main/java/com/example/didactic_app/MainActivity.kt
+++ app/src/main/java/com/example/didactic_app/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.didactic_app
 
+import android.content.Intent
 import android.graphics.Rect
 import android.location.GpsStatus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.didactic_app.databinding.ActivityMainBinding
+import com.example.didactic_app.databinding.ActivityMapBinding
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapListener
@@ -16,76 +22,28 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
-class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
+class MainActivity : AppCompatActivity() {
 
 
-
-    lateinit var mMap: MapView
-    lateinit var controller: IMapController;
-    lateinit var mMyLocationOverlay: MyLocationNewOverlay;
+    private lateinit var btnMapa: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        Configuration.getInstance().load(
-            applicationContext,
-            getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
-        )
-        mMap = binding.osmmap
-        mMap.setTileSource(TileSourceFactory.MAPNIK)
-        mMap.mapCenter
-        mMap.setMultiTouchControls(true)
-        mMap.getLocalVisibleRect(Rect())
+        setContentView(R.layout.activity_main)
 
-
-        mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
-        controller = mMap.controller
-
-        mMyLocationOverlay.enableMyLocation()
-        mMyLocationOverlay.enableFollowLocation()
-        mMyLocationOverlay.isDrawAccuracyEnabled = true
-        mMyLocationOverlay.runOnFirstFix {
-            runOnUiThread {
-                controller.setCenter(mMyLocationOverlay.myLocation);
-                controller.animateTo(mMyLocationOverlay.myLocation)
-            }
+        btnMapa = findViewById(R.id.btnMapa);
+        btnMapa.setOnClickListener { v: View ->
+            var intent: Intent = Intent(this@MainActivity, MapActivity::class.java);
+            activityResultLauncher.launch(intent)
         }
-        // val mapPoint = GeoPoint(latitude, longitude)
-
-        controller.setZoom(6.0)
-
-        Log.e("TAG", "onCreate:in ${controller.zoomIn()}")
-        Log.e("TAG", "onCreate: out  ${controller.zoomOut()}")
-
-        // controller.animateTo(mapPoint)
-        mMap.overlays.add(mMyLocationOverlay)
-
-        mMap.addMapListener(this)
-
 
     }
 
-    override fun onScroll(event: ScrollEvent?): Boolean {
-        // event?.source?.getMapCenter()
-        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
-        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
-        //  Log.e("TAG", "onScroll   x: ${event?.x}  y: ${event?.y}", )
-        return true
-    }
-
-    override fun onZoom(event: ZoomEvent?): Boolean {
-        //  event?.zoomLevel?.let { controller.setZoom(it) }
-
-
-        Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
-        return false;
-    }
-
-    override fun onGpsStatusChanged(event: Int) {
-
-
-        TODO("Not yet implemented")
-    }
-
+    private val activityResultLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // NO SE HACE NADA
+            // if (Activity.RESULT_OK == result.resultCode) {
+            //     val intent = result.data
+            // }
+        }
 
 }
