cc7e18bf1560e0ba4144edc65f6af61038423b94
Iker Gonzalez <nintzatekeen@gmail.com>
2023-12-05 09:14:40 +0100

    Movimiento Armónico Simple conseguido

--- app/src/main/java/com/example/didactic_app/LanzamientoActivity.kt
+++ app/src/main/java/com/example/didactic_app/LanzamientoActivity.kt
@@ -1,13 +1,18 @@
 package com.example.didactic_app
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import kotlin.math.abs
+import kotlin.math.sin
 
 class LanzamientoActivity : AppCompatActivity() {
     private lateinit var bolaImageView: ImageView
+    private lateinit var diana: View
+
     private var offsetX = 0f
     private var offsetY = 0f
     private var velocidadX = 0f
@@ -19,11 +24,21 @@ class LanzamientoActivity : AppCompatActivity() {
     private  var xFinal = 0f
     private  var yFinal = 0f
 
+
+    private var amplitud = 100f // Ajusta la amplitud según tus necesidades
+    private var frecuencia = 2 * Math.PI / 5000 // Ajusta la frecuencia según tus necesidades
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lanzamiento)
 
         bolaImageView = findViewById(R.id.bolaImageView)
+        diana = findViewById(R.id.diana)
+
+        amplitud = Resources.getSystem().displayMetrics.widthPixels / 2f
+
+        iniciarAnimacionMAS()
 
         bolaImageView.setOnTouchListener { _, event ->
             when (event.action) {
@@ -37,8 +52,17 @@ class LanzamientoActivity : AppCompatActivity() {
                     yInicial = event.y - offsetY
                 }
                 MotionEvent.ACTION_MOVE -> {
-                    bolaImageView.x = event.x - offsetX
-                    bolaImageView.y = event.y - offsetY
+
+                    var x = event.x - offsetX
+                    var y = event.y - offsetY
+
+                    var altura = Resources.getSystem().displayMetrics.heightPixels
+                    var limite = altura * 2 / 3
+                    if (y > limite) {
+                        bolaImageView.x = x
+                        bolaImageView.y = y
+                    }
+
                 }
                 MotionEvent.ACTION_UP -> {
                     // Calcular la velocidad basada en la diferencia de posición entre DOWN y UP
@@ -53,7 +77,7 @@ class LanzamientoActivity : AppCompatActivity() {
                     // Limitar el lanzamiento solo hacia arriba
                     if (velocidadY < 0) {
                         // Simular desaceleración reduciendo la velocidad gradualmente
-                        val factorDesaceleracion = 0.18f
+                        val factorDesaceleracion = 0.25f
                         velocidadX *= factorDesaceleracion
                         velocidadY *= factorDesaceleracion
 
@@ -71,6 +95,8 @@ class LanzamientoActivity : AppCompatActivity() {
     private fun moverBolaConVelocidad() {
         while (abs(velocidadX) > 1 || abs(velocidadY) > 1) {
             runOnUiThread {
+                velocidadX -= abs(velocidadX) * 0.05f * (velocidadX / abs(velocidadX))
+                velocidadY -= velocidadY * 0.05f
                 bolaImageView.x += velocidadX
                 bolaImageView.y += velocidadY
             }
@@ -81,5 +107,34 @@ class LanzamientoActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun iniciarAnimacionMAS() {
+        val periodo = 2000L // Ajusta el periodo según tus necesidades
+        val startTime = System.currentTimeMillis()
+
+        val runnable = object : Runnable {
+            override fun run() {
+                val currentTime = System.currentTimeMillis()
+                val deltaTime = currentTime - startTime
+
+                // Calcular la posición horizontal usando la función sin
+                val newX = amplitud * sin(frecuencia * deltaTime.toFloat())
+
+                // Establecer la posición en el eje X de la vista "diana"
+                diana.translationX = newX.toFloat()
+
+                // Programar la próxima ejecución del runnable
+                diana.postDelayed(this, 16) // Aproximadamente 60 cuadros por segundo
+
+                // Detener la animación después de un periodo específico (opcional)
+//                if (deltaTime >= periodo) {
+//                    diana.removeCallbacks(this)
+//                }
+            }
+        }
+
+        // Programar la primera ejecución del runnable
+        diana.post(runnable)
     }
 }
